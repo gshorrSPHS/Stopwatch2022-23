34ff26af9633187fe0509d3e4fad5fbcc3bcb564
gshorr <gshorr@spusd.net>
2022-09-12 09:16:56 -0700

    lifecycle methods overridden and logs added

--- app/src/main/java/com/mistershorr/stopwatch/MainActivity.kt
+++ app/src/main/java/com/mistershorr/stopwatch/MainActivity.kt
@@ -15,6 +15,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate: ")
     }
 
     override fun onStart() {
@@ -22,4 +23,28 @@ class MainActivity : AppCompatActivity() {
         Log.d(TAG, "onStart: ")
     }
 
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart: ")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume: ")
+    }
+    
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause: ")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop: ")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy: ")
+    }
 }
